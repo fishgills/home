2887c325a6ab6f5b34cda2349a6edc07cf0509b4
Charlie Davis <charlie@mimosa.co>
2016-01-22 21:21:37 -0800

    no message

--- batch/batch.py
+++ batch/batch.py
@@ -1,9 +1,26 @@
 import temperature
-import mongo
+from pymongo import MongoClient
 import ecobee
 
-weather = temperature.GetWeather()
-mongo.SaveWeather(weather)
+client = MongoClient('192.168.1.2')
 
+db = client.house
+temps = db.temps
+
+weather = temperature.GetWeather()
 ecobee = ecobee.Ecobee()
-mongo.SaveEco(ecobee)
+
+data = {}
+
+def kelvinToF(kelvin):
+    return (kelvin - 273.15) * 1.8 + 32
+
+for sensor in ecobee['thermostatList'][0]['remoteSensors']:
+    name = sensor["name"].replace(" ", "_").lower()
+    temp = float(sensor["capability"][0]["value"]) / 10
+
+    data[name] = temp
+
+data["outside"] = kelvinToF(weather["main"]["temp"])
+
+temps.insert_one(data)
